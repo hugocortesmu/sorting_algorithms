b1ff9147713656cc17052191b4a7119c42b5f3e2
Hugo Cortes <hugocortesmu@gmail.com>
2021-06-15 15:07:02 -0500

    fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, aux;
+	int i, j, aux;
 
-    for (j = 0; j < size; j++)
-    {
-        for(i = 0; i < size; i++)
-        {
-            if(i >= size-1)
-            break;
-            if (array[i] > array[i+1])
-            {
-                aux = array[i+1];
-                array[i+1] = array[i];
-                array[i] = aux;
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (i >= size - 1)
+				break;
+			if (array[i] > array[i + 1])
+			{
+				aux = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = aux;
 				print_array(array, size);
-            }
-
-        }
-
-        i = 0;
-    }
+			}
+		}
+		i = 0;
+	}
 }
